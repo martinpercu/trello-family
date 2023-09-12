e1d3647c6e66158f59e215bec3bebd7cee9eb8fe
Martin Mendez <martinpercu@gmail.com>
2023-09-12 23:37:56 +0100

    implementation to type all colors for all actual and futures components

--- src/app/modules/shared/components/card-color/card-color.component.ts
+++ src/app/modules/shared/components/card-color/card-color.component.ts
@@ -1,40 +1,17 @@
 import { Component, Input } from '@angular/core';
 
+import { Allcolors, ALLCOLORS } from '@models/colors.model';
+
+
 @Component({
   selector: 'app-card-color',
   templateUrl: './card-color.component.html'
 })
 export class CardColorComponent {
 
-  @Input() color: 'sky' | 'green' | 'violet' | 'gray' | 'yellow' = 'sky';
+  @Input() color: Allcolors = 'sky';
 
-  mapColor = {
-    sky: {
-      'bg-sky-600': true,
-      'hover:bg-sky-800': true,
-      'text-white': true
-    },
-    green: {
-      'bg-green-600': true,
-      'hover:bg-green-800': true,
-      'text-white': true
-    },
-    violet: {
-      'bg-violet-600': true,
-      'hover:bg-violet-800': true,
-      'text-white': true
-    },
-    gray: {
-      'bg-gray-600': true,
-      'hover:bg-gray-800': true,
-      'text-white': true
-    },
-    yellow: {
-      'bg-yellow-600': true,
-      'hover:bg-yellow-800': true,
-      'text-white': true
-    },
-  }
+  mapColor = ALLCOLORS;
 
   // get colors() {
   //   const classes = this.mapColor[this.color];
